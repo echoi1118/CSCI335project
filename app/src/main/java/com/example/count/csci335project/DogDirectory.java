d89daebae3f1d29d85711d1ad90047420508bc55
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-11-10 01:54:09 -0800

    Dog files placed in drawables

--- app/src/main/java/com/example/count/csci335project/DogDirectory.java
+++ app/src/main/java/com/example/count/csci335project/DogDirectory.java
@@ -11,25 +11,31 @@ public class DogDirectory extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.dogs:
-                    mTextMessage.setText(R.string.dogs);
-                    return true;
-                case R.id.favorites:
-                    mTextMessage.setText(R.string.favorites);
-                    return true;
-                case R.id.reservations:
-                    mTextMessage.setText(R.string.reservations);
-                    return true;
+    {
+        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.dogs:
+                        mTextMessage.setText(R.string.dogs);
+                        return true;
+                    case R.id.favorites:
+                        mTextMessage.setText(R.string.favorites);
+                        return true;
+                    case R.id.reservations:
+                        mTextMessage.setText(R.string.reservations);
+                        return true;
+                    case R.id.our_mission:
+                        mTextMessage.setText(R.string.our_mission);
+                        return true;
+                }
+                return false;
             }
-            return false;
-        }
-    };
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +43,14 @@ public class DogDirectory extends AppCompatActivity {
         setContentView(R.layout.activity_dog_directory);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void onItemClick(MenuItem item)
+    {
+
+
     }
 
 }
