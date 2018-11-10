791de79c4181015535b1350ae7390c122afa697f
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-11-10 00:01:09 -0800

    Dog Directory activity created and code transferred to it in order to merge

--- app/src/main/java/com/example/count/csci335project/DogDirectory.java
+++ app/src/main/java/com/example/count/csci335project/DogDirectory.java
@@ -3,19 +3,11 @@ package com.example.count.csci335project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class DogDirectory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dog_directory);
     }
-
-
-
-
-
-
 }
-
