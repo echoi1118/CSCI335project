254c93bcd30c25f386414293551ed5693e977577
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-11-10 01:04:04 -0800

    Merge hopefully successful

---
+++ app/src/main/java/com/example/count/csci335project/DogDirectory.java
@@ -0,0 +1,44 @@
+package com.example.count.csci335project;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DogDirectory extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.dogs:
+                    mTextMessage.setText(R.string.dogs);
+                    return true;
+                case R.id.favorites:
+                    mTextMessage.setText(R.string.favorites);
+                    return true;
+                case R.id.reservations:
+                    mTextMessage.setText(R.string.reservations);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_directory);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
