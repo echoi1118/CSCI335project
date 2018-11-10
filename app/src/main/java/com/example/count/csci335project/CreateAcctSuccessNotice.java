0449d2223c26df2d8f327fd0c9502143d831675d
Esther <estherchoi012@gmail.com>
2018-11-10 15:18:16 -0800

    Everything prior to reservation stuff getting linked

--- app/src/main/java/com/example/count/csci335project/CreateAcctSuccessNotice.java
+++ app/src/main/java/com/example/count/csci335project/CreateAcctSuccessNotice.java
@@ -1,7 +1,9 @@
 package com.example.count.csci335project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAcctSuccessNotice extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class CreateAcctSuccessNotice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acct_success_notice);
+    }
+
+    /** Called when the user clicks the text to return them to the login screen */
+    public void goToLoginScreen(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
