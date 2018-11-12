96c92e48c437a435386736676fb3ff34f98173e0
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-11-12 13:22:27 -0800

    Bottom nav workafound implemented on dog directory page and partialy on donate and beyonce pages

--- app/src/main/java/com/example/count/csci335project/ReservationActivity.java
+++ app/src/main/java/com/example/count/csci335project/ReservationActivity.java
@@ -5,11 +5,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.icu.util.Calendar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class ReservationActivity extends AppCompatActivity {
