e9ba380e8c47311f792ca400f2a9abff9631cbfe
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-11-11 21:02:48 -0800

    My reservations activity created without button linking

---
+++ app/src/main/java/com/example/count/csci335project/MyReservations.java
@@ -0,0 +1,13 @@
+package com.example.count.csci335project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyReservations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_reservations);
+    }
+}
