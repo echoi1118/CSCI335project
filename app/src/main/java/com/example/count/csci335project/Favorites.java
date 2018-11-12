3940e43d3a24e9302a82e0fd70de8598eabd1ab2
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-11-11 17:00:13 -0800

    Favorites sample page with dogs Selena and Kanye shown

---
+++ app/src/main/java/com/example/count/csci335project/Favorites.java
@@ -0,0 +1,62 @@
+package com.example.count.csci335project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class Favorites extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+
+    {
+        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.dogs:
+                        mTextMessage.setText(R.string.dogs);
+                        return true;
+                    case R.id.favorites:
+                        mTextMessage.setText(R.string.favorites);
+                        return true;
+                    case R.id.reservations:
+                        mTextMessage.setText(R.string.reservations);
+                        return true;
+                    case R.id.our_mission:
+                        mTextMessage.setText(R.string.our_mission);
+                        return true;
+                }
+                return false;
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void onItemClick(MenuItem item) {
+
+
+    }
+
+    public void goToDogDirectory(View view) {
+        Intent intent = new Intent(this, DogDirectory.class);
+        startActivity(intent);
+    }
+
+}
