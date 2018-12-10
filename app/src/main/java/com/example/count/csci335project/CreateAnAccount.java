b9d0efa3810ea82976a5867a1adc9d61d4c8a77e
camobro387 <35618898+camobro387@users.noreply.github.com>
2018-12-09 17:47:21 -0800

    Email and password validation implemented although no accounts are actually made.

--- app/src/main/java/com/example/count/csci335project/CreateAnAccount.java
+++ app/src/main/java/com/example/count/csci335project/CreateAnAccount.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CreateAnAccount extends AppCompatActivity {
 
@@ -11,11 +15,52 @@ public class CreateAnAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_an_account);
+
     }
 
     /** Called when the user clicks the "Create" (account) button */
     public void goToCreateAccountSuccessPage(View view) {
-        Intent intent = new Intent(this, CreateAcctSuccessNotice.class);
-        startActivity(intent);
+        EditText validation = (EditText) findViewById(R.id.create_acct_email);
+        EditText pass1val = (EditText) findViewById(R.id.create_acct_pw);
+        EditText pass2val = (EditText) findViewById(R.id.create_acct_pw_confirm);
+        if(!isValidEmail(validation.getText()))
+        {
+            validation.setError("Please enter a valid email.");
+        }
+        else if(!isValidPassword(pass1val.getText()))
+        {
+            pass1val.setError("Please enter a valid password.");
+
+        }
+        else if(pass1val.getText().equals(pass2val.getText()))
+        {
+            pass2val.setError("Please enter a matching password.");
+        }
+        else {
+            Intent intent = new Intent(this, CreateAcctSuccessNotice.class);
+            startActivity(intent);
+        }
     }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null)
+            return false;
+
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public final static boolean isValidPassword(CharSequence password) {
+
+        Pattern pattern;
+        Matcher matcher;
+
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
+    }
+
 }
--- app/src/main/java/com/example/count/csci335project/MainActivity.java
+++ app/src/main/java/com/example/count/csci335project/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     /** Called when the user clicks the Create an Account link */
@@ -20,14 +27,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToDogDirectory(View view){
+        EditText validation = (EditText) findViewById(R.id.email_login_field);
+        EditText passValid = (EditText) findViewById(R.id.pw_login_field);
+        if(!isValidEmail(validation.getText()))
+        {
+            validation.setError("Please enter a valid email.");
+        }
+        else if(!isValidPassword(passValid.getText()))
+        {
+            passValid.setError("Please enter a valid password.");
+        }
+        else {
+            Intent intent = new Intent(this, DogDirectory.class);
+            startActivity(intent);
+        }
 
-
-
-
-        Intent intent = new Intent(this, DogDirectory.class);
-        startActivity(intent);
     }
 
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null)
+            return false;
 
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public final static boolean isValidPassword(CharSequence password) {
+
+        Pattern pattern;
+        Matcher matcher;
+
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
+    }
 
 }
